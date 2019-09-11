b7883bce551914fe382794880c019e4d372fcb73
omar hussain <omar.hussain4991@gmail.com>
2019-09-11 23:59:36 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -19,10 +19,8 @@ return age <= 19 && age >= 13 ? "You are a teenager" : "You are not a teenager"
 
 function switchAge(age) {
 switch (age) {
-  case expression: age <= 19 && age >= 13
-    console.log("You are a teenager")
-    break;
-  default:
-  console.log ("You have an age")
-}
+case age <= 19 && age >= 13
+return "You are a teenager"
+default
+return "You have an age"}
 }

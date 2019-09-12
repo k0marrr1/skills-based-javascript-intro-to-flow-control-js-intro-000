68c4d84f37c193e30bb4c372603af96bff702a7d
omar hussain <omar.hussain4991@gmail.com>
2019-09-12 00:03:34 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -18,8 +18,12 @@ return age <= 19 && age >= 13 ? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
-switch(age) {
-  case age <= 19 && age >= 13
-  return ("You are a teenager")
+switch (age) {
+  case age <= 19 && age >= 13:
+    return ("You are a teenager")
+    break;
+  default:
+  return ("You have an age")
+
 }
 }
